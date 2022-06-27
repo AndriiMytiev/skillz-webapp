63af6a8592c0cf5e21b9221f7ee1f26667c32029
Aleksandra Hrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2022-06-27 20:51:27 +0300

    added workshops data

---
+++ data/workshops.js
@@ -0,0 +1,56 @@
+export default [
+  {
+    id: 'workshop-1',
+    title: 'Photography Basics',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, commodi numquam! Dolorem nesciunt cum quibusdam voluptas facilis animi iure architecto dolorum, perspiciatis sint, veritatis corrupti. Ex rem tempore possimus illo?',
+    linkLabel: 'learn more'
+  },
+  {
+    id: 'workshop-2',
+    title: 'Business 101',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, commodi numquam! Dolorem nesciunt cum quibusdam voluptas facilis animi iure architecto dolorum, perspiciatis sint, veritatis corrupti. Ex rem tempore possimus illo?',
+    linkLabel: 'learn more'
+  },
+  {
+    id: 'workshop-3',
+    title: 'Social Networking Camp',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, commodi numquam! Dolorem nesciunt cum quibusdam voluptas facilis animi iure architecto dolorum, perspiciatis sint, veritatis corrupti. Ex rem tempore possimus illo?',
+    linkLabel: 'learn more'
+  },
+  {
+    id: 'workshop-4',
+    title: 'Photography Basics',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, commodi numquam! Dolorem nesciunt cum quibusdam voluptas facilis animi iure architecto dolorum, perspiciatis sint, veritatis corrupti. Ex rem tempore possimus illo?',
+    linkLabel: 'learn more'
+  },
+  {
+    id: 'workshop-5',
+    title: 'Business 101',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, commodi numquam! Dolorem nesciunt cum quibusdam voluptas facilis animi iure architecto dolorum, perspiciatis sint, veritatis corrupti. Ex rem tempore possimus illo?',
+    linkLabel: 'learn more'
+  },
+  {
+    id: 'workshop-6',
+    title: 'Social Networking Camp',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, commodi numquam! Dolorem nesciunt cum quibusdam voluptas facilis animi iure architecto dolorum, perspiciatis sint, veritatis corrupti. Ex rem tempore possimus illo?',
+    linkLabel: 'learn more'
+  },
+  {
+    id: 'workshop-7',
+    title: 'Photography Basics',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, commodi numquam! Dolorem nesciunt cum quibusdam voluptas facilis animi iure architecto dolorum, perspiciatis sint, veritatis corrupti. Ex rem tempore possimus illo?',
+    linkLabel: 'learn more'
+  },
+  {
+    id: 'workshop-8',
+    title: 'Business 101',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, commodi numquam! Dolorem nesciunt cum quibusdam voluptas facilis animi iure architecto dolorum, perspiciatis sint, veritatis corrupti. Ex rem tempore possimus illo?',
+    linkLabel: 'learn more'
+  },
+  {
+    id: 'workshop-9',
+    title: 'Social Networking Camp',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, commodi numquam! Dolorem nesciunt cum quibusdam voluptas facilis animi iure architecto dolorum, perspiciatis sint, veritatis corrupti. Ex rem tempore possimus illo?',
+    linkLabel: 'learn more'
+  }
+]
