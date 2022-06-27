abce34a5beb27272dbe55117145f0cedee4115ce
Aleksandra Hrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2022-06-28 00:29:23 +0300

    added articles data

---
+++ data/articles.js
@@ -0,0 +1,45 @@
+export default [
+  {
+    id: "article-1",
+    image: "",
+    createdAt: 1612735200000,
+    label: 'The TikTok Algorithm: How to Get the Best Engagement',
+    author: 'Dianne Russell'
+  },
+  {
+    id: "article-2",
+    image: "",
+    createdAt: 1612735200000,
+    label: 'How to Win Friends and Influence People',
+    author: 'MArvin McKinney Russell'
+  },
+  {
+    id: "article-3",
+    image: "",
+    createdAt: 1612044000000,
+    label: 'iPhone Photography: Tips, Tricks and Best Practices',
+    author: 'Katie Murphy'
+  },
+  {
+    id: "article-4",
+    image: "",
+    createdAt: 1612735200000,
+    label: 'The TikTok Algorithm: How to Get the Best Engagement',
+    author: 'Dianne Russell'
+  },
+  {
+    id: "article-5",
+    image: "",
+    createdAt: 1612735200000,
+    label: 'How to Win Friends and Influence People',
+    author: 'MArvin McKinney Russell'
+  },
+  {
+    id: "article-6",
+    image: "",
+    createdAt: 1612044000000,
+    label: 'iPhone Photography: Tips, Tricks and Best Practices',
+    author: 'Katie Murphy'
+  }
+]
+
