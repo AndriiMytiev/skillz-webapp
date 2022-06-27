de3413b85cfed85d53502faf55ebed94b39033ef
Aleksandra Hrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2022-06-27 20:55:34 +0300

    added comments data

---
+++ data/comments.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: "1",
+    score: 5,
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, commodi numquam! Dolorem nesciunt cum quibusdam voluptas facilis animi iure architecto dolorum, perspiciatis sint, veritatis corrupti. Ex rem tempore possimus illo?',
+    author: 'James Brown',
+    role: 'influencer'
+  },
+  {
+    id: "2",
+    score: 4,
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, commodi numquam! Dolorem nesciunt cum quibusdam voluptas facilis animi iure architecto dolorum, perspiciatis sint, veritatis corrupti. Ex rem tempore possimus illo?',
+    author: 'Nick Cave',
+    role: 'developer'
+  },
+  {
+    id: "3",
+    score: 3,
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, commodi numquam! Dolorem nesciunt cum quibusdam voluptas facilis animi iure architecto dolorum, perspiciatis sint, veritatis corrupti. Ex rem tempore possimus illo?',
+    author: 'Steve Jobs',
+    role: 'SEO'
+  },
+  {
+    id: "4",
+    score: 5,
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, commodi numquam! Dolorem nesciunt cum quibusdam voluptas facilis animi iure architecto dolorum, perspiciatis sint, veritatis corrupti. Ex rem tempore possimus illo?',
+    author: 'James Brown',
+    role: 'influencer'
+  },
+  {
+    id: "5",
+    score: 4,
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, commodi numquam! Dolorem nesciunt cum quibusdam voluptas facilis animi iure architecto dolorum, perspiciatis sint, veritatis corrupti. Ex rem tempore possimus illo?',
+    author: 'Nick Cave',
+    role: 'developer'
+  },
+  {
+    id: "6",
+    score: 3,
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, commodi numquam! Dolorem nesciunt cum quibusdam voluptas facilis animi iure architecto dolorum, perspiciatis sint, veritatis corrupti. Ex rem tempore possimus illo?',
+    author: 'Steve Jobs',
+    role: 'SEO'
+  }
+]
