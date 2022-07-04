b29035712cd57a62e0dd9fd52e6260c82103a69b
Rtpoy <104576500+Rtpoy@users.noreply.github.com>
2022-07-04 18:47:55 +0300

    add all features

--- js/cors.js
+++ js/cors.js
@@ -3,6 +3,10 @@ import refs from './refs.js';
 
 const {overlay, content} = refs;
 
+const newModalElement = `<div class='coursesModalBlock'>
+<ul class='courseModalList'></u>
+</div>`;
+
 const newElement = `<div class='coursesListBlock'>
                         <ul class='courseList'></u>
                     </div>`;
@@ -28,18 +32,48 @@ function createElement(id){
 
 
 
+const openModal = document.querySelector('.button.btnViewAll');
+
+openModal.addEventListener('click', () => {
+    content.style.width="fit-content";
+    content.style.height="fit-content";
+  overlay.classList.remove('ifHidden')  
+  document.querySelector('.content').insertAdjacentHTML('afterbegin', newModalElement);
+  const list =  createModalList(data);
+  document.querySelector('.courseModalList').insertAdjacentHTML('beforeend', list);
+
+})
+
+overlay.addEventListener("click", (event) =>{
+    if(
+    event.target.classList.contains('overlay') ){ overlay.classList.add('ifHidden');
+    content.removeChild(document.querySelector('.coursesModalBlock'));
+    content.removeAttribute('style');
+ }})
+
+window.addEventListener('keydown', (e) =>{
+  console.log("DOWN", e.code, e.key);
+  console.log(e.code === 'Escape');
+if(e.code === 'Escape'){
+  overlay.classList.add('ifHidden');
+}
+
+
+  e.code === 'Escape' && overlay.classList.add('ifHidden');
+})
 
 
 
-//modalwindow
-//function createModalList(data) { 
-    // return data.map((el) => {
-        // return `<li class='${el.id}'>
-                    // <img src='${el.image}' width='200px'>
-                    // <p>${el.prices.regularPrice}</p>
-                    // <p>${el.label}</p>
-                    // <p>${el.tutor}</p>
-                    // <button type='button'>${el.linkLabel}</button>
-                // </li>`
-        // }).join(''); 
-// }
+
+
+function createModalList(data) { 
+    return data.map((el) => {
+         return `<li class='${el.id}' wi>
+                    <img src='${el.image}' width='393px height="197"'>
+                    <p>${el.prices.regularPrice}</p>
+                     <p>${el.label}</p>
+                    <p>${el.tutor}</p>
+                     <button type='button'>${el.linkLabel}</button>
+                 </li>`
+         }).join(''); 
+}
