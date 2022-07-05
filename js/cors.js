78cf3914e8455d588608b14a4ecddf7749ac7fdd
Rtpoy <104576500+Rtpoy@users.noreply.github.com>
2022-07-05 19:21:01 +0300

    add all features

--- js/cors.js
+++ js/cors.js
@@ -1,83 +1,34 @@
-import data from '../data/courses.js';
-import refs from './refs.js';
-import createList from './helpers/createList.js';
+import data from "../data/courses.js";
+import refs from "./refs.js";
+import createList from "./helpers/createList.js";
 
-const {overlay, content} = refs;
+const { overlay, content } = refs;
+const newElement = `<div class='coursesListBlock'></div>`;
+document.getElementById("courses").insertAdjacentHTML("beforeend", newElement);
+const previewArray = data.slice(0, 3);
+const previewList = createList(previewArray, createItem, "coursesList");
+const coursesBlock = document.querySelector(".coursesListBlock");
+coursesBlock.insertAdjacentHTML("afterbegin", previewList);
+const openModal = document.querySelector(".button.btnViewAll");
 
-const newModalElement = `<div class='coursesModalBlock'>
-<ul class='courseModalList'></u>
-</div>`;
+openModal.addEventListener("click", () => {
+  const list = createList(data, createItem, "coursesModalList");
+  content.insertAdjacentHTML("afterbegin", list);
+  overlay.classList.remove("ifHidden");
+});
 
-const newElement = `<div class='coursesListBlock'>
-                        <ul class='courseList'></u>
-                    </div>`;
-
-document.getElementById('courses').insertAdjacentHTML('beforeend', newElement);
-
-// for(let i = 0; i < 3; i++){
-//     let listElement = createElement(i);
-//     document.querySelector('.courseList').insertAdjacentHTML('beforeend', listElement);
-// }
-const previewArray = data.slice(0, 3)
-console.log(previewArray);
-const previewList = createList(previewArray, createItem)
-console.log(previewList)
-document.querySelector('.courseList').insertAdjacentHTML('beforeend', previewList);
-// function createElement(id){
-//     return `<li class='${data[id].id}'>
-//                 <img src='${data[id].image}' width='393px'>
-//                 <div class="paddingText">
-//                 <p class="label">${data[id].label}</p>
-//                 <p>${data[id].tutor}</p>
-//                 <p class="price">${data[id].prices.regularPrice}</p>
-//                 <button type='button' class='buttonMore'>${data[id].linkLabel}</button>
-//                 </div>
-//             </li>`
-// }
-
-
-
-const openModal = document.querySelector('.button.btnViewAll');
-
-openModal.addEventListener('click', () => {
-  overlay.classList.remove('ifHidden')  
-  document.querySelector('.content').insertAdjacentHTML('afterbegin', newModalElement);
-  const list =  createList(data, createItem);
-  document.querySelector('.courseModalList').insertAdjacentHTML('beforeend', list);
-
-})
-
-overlay.addEventListener("click", (event) =>{
-    if(
-    event.target.classList.contains('overlay') ){ overlay.classList.add('ifHidden');
-    content.removeChild(document.querySelector('.coursesModalBlock'));
-    content.removeAttribute('style');
- }})
-
-window.addEventListener('keydown', (e) =>{
-  console.log("DOWN", e.code, e.key);
-  console.log(e.code === 'Escape');
-if(e.code === 'Escape'){
-  overlay.classList.add('ifHidden');
-}
-
-
-  e.code === 'Escape' && overlay.classList.add('ifHidden');
-})
-
-
-
-
-
-// function createModalList(data,createElement) { 
-//   return data.map(el => createElement(el)).join("");
-// }
+overlay.addEventListener("click", (event) => {
+  if (event.target.classList.contains("overlay")) {
+    overlay.classList.add("ifHidden");
+    content.removeChild(document.querySelector(".coursesModalList"));
+  }
+});
 function createItem(el) {
   return `<li class='${el.id}'>
-  <img src='${el.image}' width='393px height="197"'>
-  <p>${el.prices.regularPrice}</p>
-   <p>${el.label}</p>
-  <p>${el.tutor}</p>
+  <img src='${el.image}' width='373px'>
+  <p clas="price">${el.prices.regularPrice}</p>
+   <p class="label">${el.label}</p>
+  <p class="tutor">${el.tutor}</p>
    <button type='button'>${el.linkLabel}</button>
-</li>`
+</li>`;
 }
--- js/helpers/createList.js
+++ js/helpers/createList.js
@@ -1,3 +1,4 @@
-export default function createList(array,callback){
-    return array.map(item => callback(item)).join("");
+export default function createListMarkup(array, callback, className) {
+  const items = array.map((item) => callback(item)).join("");
+  return `<ul class="${className}">${items}</ul>`;
 }
