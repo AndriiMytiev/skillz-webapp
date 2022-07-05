6e2949c69f3b07e87a91c66504374aa36f362171
Rtpoy <104576500+Rtpoy@users.noreply.github.com>
2022-07-05 19:53:14 +0300

    add all workshop

--- js/work.js
+++ js/work.js
@@ -1,74 +1,29 @@
-import data from '../data/workshops.js';
-import refs from './refs.js';
+import data from "../data/workshops.js";
+import createListMarkup from "./helpers/createList.js";
+import refs from "./refs.js";
 
-const {overlay, content} = refs;
+const { overlay, content, workshopBlock } = refs;
+const previewArray = data.slice(0, 3);
+const previewList = createListMarkup(previewArray, createItem, "workshopList");
+workshopBlock.insertAdjacentHTML("afterbegin", previewList);
+const openModal = document.querySelector(".button.btnViewAllW");
 
-const newModalElement = `<div class='workshopModalBlock'>
-<ul class='workshopModalList'></u>
-</div>`;
+openModal.addEventListener("click", () => {
+  const list = createListMarkup(data, createItem, "workshopModalList");
+  content.insertAdjacentHTML("afterbegin", list);
+  overlay.classList.remove("ifHidden");
+});
+overlay.addEventListener("click", (event) => {
+  if (event.target.classList.contains("overlay")) {
+    overlay.classList.add("ifHidden");
+    content.removeChild(document.querySelector(".workshopModalList"));
+  }
+});
 
-const newElement = `<div class='workshopListBlock'>
-                        <ul class='workshopList'></u>
-                    </div>`;
-
-document.getElementById('workshops').insertAdjacentHTML('beforeend', newElement);
-
-for(let i = 0; i < 3; i++){
-    let listElement = createElement(i);
-    document.querySelector('.workshopList').insertAdjacentHTML('beforeend', listElement);
-}
-
-function createElement(id){
-    return `<li class='${data[id].id}'>
-                <div class="paddingText">
-                <h1 class="title">${data[id].title}</h1>
-                <p class="description">${data[id].description}</p>
-                <button type='button' class='buttonMore'>${data[id].linkLabel}</button>
-                </div>
-            </li>`
-}
-
-
-
- const openModal = document.querySelector('.button.btnViewAllW');
-
- openModal.addEventListener('click', () => {
-    content.style.width="fit-content";
-    content.style.height="fit-content";
-    overlay.classList.remove('ifHidden')  
-   document.querySelector('.content').insertAdjacentHTML('afterbegin', newModalElement);
-   const list =  createModalList(data);
-   document.querySelector('.workshopModalList').insertAdjacentHTML('beforeend', list);
- })
-
- overlay.addEventListener("click", (event) =>{
-   if(
-     event.target.workshopList.contains('overlay') ){ overlay.classList.add('ifHidden');
-     content.removeChild(document.querySelector('.workshopModalBlock'));
-     content.removeAttribute('style');
-  }})
-
- window.addEventListener('keydown', (e) =>{
-   console.log("DOWN", e.code, e.key);
-   console.log(e.code === 'Escape');
- if(e.code === 'Escape'){
-   overlay.workshopList.add('ifHidden');
- }
-
-
-   e.code === 'Escape' && overlay.workshopList.add('ifHidden');
- })
-
-
-
-
-
- function createModalList(data) { 
-     return data.map((el) => {
-          return `<li class='${el.id}'>
+function createItem(el) {
+  return `<li class='${el.id}'>
                       <h1>${el.title}</h1>
                       <p>${el.description}</p>
-                      <button type='button'>${el.linkLabel}</button>
-                  </li>`
-          }).join(''); 
+                       <button type='button'>${el.linkLabel}</button>
+                  </li>`;
 }
