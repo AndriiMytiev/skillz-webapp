e60c5683b94a62ba2c55cca53c7640b7c6b2485f
AndriiMytiev <79248454+AndriiMytiev@users.noreply.github.com>
2022-07-07 16:58:54 +0300

    fixed modal for contact us

--- js/helpers/hideModalWindow.js
+++ js/helpers/hideModalWindow.js
@@ -4,6 +4,5 @@ const {overlay, content} = refs;
 export default function hideModalWindow(elementClass){
     overlay.classList.add('ifHidden');
     content.classList.remove('toShowModalContent');
-    console.log(content.children);
     content.removeChild(document.querySelector(`.${elementClass}`));
 }
--- js/modalHeroAbout.js
+++ js/modalHeroAbout.js
@@ -1,112 +1,101 @@
 import refs from "./refs.js";
 
 const {
-    btnLearnMore, btnAboutSkills, btnCloseModal, btnContactUs,
-    modal, modalWrap, modalAbout, modalAboutWrap, modalContactUs, modalContactUsWrap,
-    act,
+  btnLearnMore,
+  btnAboutSkills,
+  btnCloseModal,
+  btnContactUs,
+  modal,
+  modalWrap,
+  modalAbout,
+  modalAboutWrap,
+  modalContactUs,
+  modalContactUsWrap,
+  act,
 } = refs;
 
-btnLearnMore.addEventListener('click', showModal);
+btnLearnMore.addEventListener("click", showModal);
 
-btnAboutSkills.addEventListener('click', showModalAbout);
+btnAboutSkills.addEventListener("click", showModalAbout);
 
-btnContactUs.addEventListener('click', showModalContactUs);
+btnContactUs.addEventListener("click", showModalContactUs);
 
+function showModal(e) {
+  modal.style.opacity = "1";
+  modal.style.transition = "all 300ms ease-in-out";
 
-function showModal(e){
+  modal.style.pointerEvents = "auto";
+  modalWrap.style.opacity = "1";
+  modalWrap.style.transform = "scale(1)";
 
-    modal.style.opacity = '1';
-    modal.style.transition = 'all 300ms ease-in-out';
-
-    modal.style.pointerEvents = "auto";
-    modalWrap.style.opacity = '1';
-    modalWrap.style.transform = 'scale(1)';
-
-    modalWrap.style.transition = 'opacity 250ms 500ms ease, transform 350ms 500ms ease';
-    // if(!e.target.closest('.btnLEarnMore')){
-    // console.log(e.target);}
-    console.log('modal');
-};
-
-function showModalAbout(e){
-
-    // console.log(act);
-    // return;
-    modalAbout.style.opacity = '1';
-    modalAbout.style.transition = 'all 300ms ease-in-out';
-
-    modalAbout.style.pointerEvents = "auto";
-    modalAboutWrap.style.opacity = '1';
-    modalAboutWrap.style.transform = 'scale(1)';
-
-    modalAboutWrap.style.transition = 'opacity 250ms 500ms ease, transform 350ms 500ms ease';
-    // if(!e.target.closest('.btnLEarnMore')){
-    // console.log(e.target);}
-    console.log('modalAbout');
-};
-
-function showModalContactUs(e){
-
-    // console.log(act);
-    // return;
-    modalContactUs.style.opacity = '1';
-    modalContactUs.style.transition = 'all 300ms ease-in-out';
-
-    modalContactUs.style.pointerEvents = "auto";
-    modalContactUsWrap.style.opacity = '1';
-    modalContactUsWrap.style.transform = 'scale(1)';
-
-    modalContactUsWrap.style.transition = 'opacity 250ms 500ms ease, transform 350ms 500ms ease';
-    // if(!e.target.closest('.btnLEarnMore')){
-    // console.log(e.target);}
-    console.log('modalAbout');
-};
-
-function closeModal(e){
-    modal.style.opacity = '0';
-    modalAbout.style.opacity= '0';
-    modalContactUs.style.opacity= '0';
-    modal.style.transition = 'opacity 250ms 700ms ease';
-    modal.style.pointerEvents = "none";
-    modalAbout.style.transition = 'opacity 250ms 700ms ease';
-    modalAbout.style.pointerEvents = "none";
-    modalContactUs.style.transition = 'opacity 250ms 700ms ease';
-    modalContactUs.style.pointerEvents = "none";
+  modalWrap.style.transition =
+    "opacity 250ms 500ms ease, transform 350ms 500ms ease";
 }
 
-document.addEventListener('keydown', (e) =>{
-    if(e.keyCode == 27){
-        closeModal();
-    }
+function showModalAbout(e) {
+  modalAbout.style.opacity = "1";
+  modalAbout.style.transition = "all 300ms ease-in-out";
+
+  modalAbout.style.pointerEvents = "auto";
+  modalAboutWrap.style.opacity = "1";
+  modalAboutWrap.style.transform = "scale(1)";
+
+  modalAboutWrap.style.transition =
+    "opacity 250ms 500ms ease, transform 350ms 500ms ease";
+}
+
+function showModalContactUs(e) {
+  modalContactUs.style.opacity = "1";
+  modalContactUs.style.transition = "all 300ms ease-in-out";
+
+  modalContactUs.style.pointerEvents = "auto";
+  modalContactUsWrap.style.opacity = "1";
+  modalContactUsWrap.style.transform = "scale(1)";
+
+  modalContactUsWrap.style.transition =
+    "opacity 250ms 500ms ease, transform 350ms 500ms ease";
+}
+
+function closeModal(e) {
+  modal.style.opacity = "0";
+  modalAbout.style.opacity = "0";
+  modalContactUs.style.opacity = "0";
+  modal.style.transition = "opacity 250ms 700ms ease";
+  modal.style.pointerEvents = "none";
+  modalAbout.style.transition = "opacity 250ms 700ms ease";
+  modalAbout.style.pointerEvents = "none";
+  modalContactUs.style.transition = "opacity 250ms 700ms ease";
+  modalContactUs.style.pointerEvents = "none";
+}
+
+document.addEventListener("keydown", (e) => {
+  if (e.keyCode == 27) {
+    closeModal();
+  }
 });
 
-
+//Close modal by click on place around modal wrapper
+modal.addEventListener("click", closeModal, { capture: false });
 
 //Close modal by click on place around modal wrapper
-modal.addEventListener('click', closeModal, {"capture": false});
+modalAbout.addEventListener("click", closeModal, { capture: false });
 
 //Close modal by click on place around modal wrapper
-modalAbout.addEventListener('click', closeModal, {"capture": false});
-
-//Close modal by click on place around modal wrapper
-modalContactUs.addEventListener('click', closeModal, {"capture": false});
+modalContactUs.addEventListener("click", closeModal, { capture: false });
 
 //Close modal by click on close modal button
-btnCloseModal.addEventListener('click', closeModal, {"capture": false});
+btnCloseModal.addEventListener("click", closeModal, { capture: false });
 
 //prevent modal close by click on modal wrapper
-modalWrap.addEventListener('click', (e) =>{
-    // console.log('modalWrap');
-    e.stopPropagation();
+modalWrap.addEventListener("click", (e) => {
+  e.stopPropagation();
 });
 
 //prevent modal close by click on modal wrapper
-modalAboutWrap.addEventListener('click', (e) =>{
-    // console.log('modalWrap');
-    e.stopPropagation();
+modalAboutWrap.addEventListener("click", (e) => {
+  e.stopPropagation();
 });
 
-modalContactUsWrap.addEventListener('click', (e) =>{
-    // console.log('modalWrap');
-    e.stopPropagation();
+modalContactUsWrap.addEventListener("click", (e) => {
+  e.stopPropagation();
 });
